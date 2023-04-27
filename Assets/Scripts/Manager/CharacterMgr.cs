6aff96a45482f86e87563669d3b3f3e6b75bfba5
JoshuaRabbit <32034557+LUOHONGPENG@users.noreply.github.com>
2023-04-27 13:20:47 +0100

    LoadPic

---
+++ Assets/Scripts/Manager/CharacterMgr.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMgr : MonoBehaviour
+{
+    public PatientModel patientModel;
+
+    public SpriteRenderer srHead;
+
+    public SpriteRenderer srBody;
+
+    public void Init()
+    {
+
+    }
+}
